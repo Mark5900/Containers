afde3aabeb56247cb04ee1b77131edc15a5b6625
Mark5900 <56847489+Mark5900@users.noreply.github.com>
2022-11-19 23:32:48 +0100

    Create GraphFun.ps1

---
+++ GraphFun.ps1
@@ -0,0 +1,25 @@
+<#
+    .NOTES
+    ===========================================================================
+    Created with: 	Visual Studio Code
+    Created on:   	
+    Created by:   	Mark5900
+    Organization: 	
+    Filename:     	
+    ===========================================================================
+    .DESCRIPTION
+        A description of the file.
+#>
+##################
+### PARAMETERS ###
+##################
+# DO NOT CHANGE
+# Change as needed
+
+#################
+### FUNCTIONS ###
+#################
+
+##############
+### SCRIPT ###
+##############
